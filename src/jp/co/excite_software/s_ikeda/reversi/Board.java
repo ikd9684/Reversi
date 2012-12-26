ead33efce863c6a97015f5a2984b04e50ebe3365
ikd9684 <ikd9684@gmail.com>
2012-12-27 00:18:19 +0900

    初期コミット

---
+++ src/jp/co/excite_software/s_ikeda/reversi/Board.java
@@ -0,0 +1,21 @@
+package jp.co.excite_software.s_ikeda.reversi;
+
+/**
+ * @author ikd9684
+ *
+ */
+public class Board {
+
+    /**  */
+    enum Disc { WHITE, BLACK };
+
+    /**  */
+    private static final String LN = System.getProperty("line.separator");
+
+    private Disc[][] status;
+
+    public Board() {
+    }
+
+    
+}
---
+++ src/jp/co/excite_software/s_ikeda/reversi/Reversi.java
@@ -0,0 +1,5 @@
+package jp.co.excite_software.s_ikeda.reversi;
+
+public class Reversi {
+
+}
